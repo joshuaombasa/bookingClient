b150f4d771eb2952f70c3d2d0014e2bf50b54288
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-03 22:59:23 +0300

    added searchResults page

---
+++ src/pages/searchresults/Searchresults.jsx
@@ -0,0 +1,227 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './searchresults.css'
+import { PiLeafFill } from 'react-icons/pi'
+import { BsStarFill } from 'react-icons/bs'
+import {TiTick} from 'react-icons/ti'
+
+export default function Searchresults() {
+
+    const results = [
+        {
+            imageUrl: "https://images.unsplash.com/photo-1616423841125-8307665a0469?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y2FuY3VufGVufDB8fDB8fHww",
+            id: '1',
+            name: 'Harbor Heights Hotel',
+            rating: '4',
+            center: 'Colorado',
+            distanceFromCenter: '4km',
+            sustainableLevel: '3',
+            beType: '1 extra-large double bed',
+            cost: '12000',
+            texes: '1200'
+        },
+        {
+            imageUrl: "https://images.unsplash.com/photo-1616423841125-8307665a0469?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y2FuY3VufGVufDB8fDB8fHww",
+            id: '1',
+            name: 'Harbor Heights Hotel',
+            rating: '4',
+            center: 'Colorado',
+            distanceFromCenter: '4km',
+            sustainableLevel: '3',
+            beType: '1 extra-large double bed',
+            cost: '12000',
+            texes: '1200'
+        },
+        {
+            imageUrl: "https://images.unsplash.com/photo-1589705149724-1379ec03e196?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHZhY2F0aW9ufGVufDB8fDB8fHww",
+            id: '1',
+            name: 'Harbor Heights Hotel',
+            rating: '4',
+            center: 'Colorado',
+            distanceFromCenter: '4km',
+            sustainableLevel: '3',
+            beType: '1 extra-large double bed',
+            cost: '12000',
+            texes: '1200'
+        },
+        {
+            imageUrl: "https://images.unsplash.com/photo-1589705149724-1379ec03e196?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHZhY2F0aW9ufGVufDB8fDB8fHww",
+            id: '1',
+            name: 'Harbor Heights Hotel',
+            rating: '4',
+            center: 'Colorado',
+            distanceFromCenter: '4km',
+            sustainableLevel: '3',
+            beType: '1 extra-large double bed',
+            cost: '12000',
+            texes: '1200'
+        },
+        {
+            imageUrl: "https://images.unsplash.com/photo-1589705149724-1379ec03e196?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHZhY2F0aW9ufGVufDB8fDB8fHww",
+            id: '1',
+            name: 'Harbor Heights Hotel',
+            rating: '4',
+            center: 'Colorado',
+            distanceFromCenter: '4km',
+            sustainableLevel: '3',
+            beType: '1 extra-large double bed',
+            cost: '12000',
+            texes: '1200'
+        },
+        {
+            imageUrl: "https://images.unsplash.com/photo-1589705149724-1379ec03e196?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHZhY2F0aW9ufGVufDB8fDB8fHww",
+            id: '1',
+            name: 'Harbor Heights Hotel',
+            rating: '4',
+            center: 'Colorado',
+            distanceFromCenter: '4km',
+            sustainableLevel: '3',
+            beType: '1 extra-large double bed',
+            cost: '12000',
+            texes: '1200'
+        },
+        {
+            imageUrl: "https://images.unsplash.com/photo-1589705149724-1379ec03e196?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHZhY2F0aW9ufGVufDB8fDB8fHww",
+            id: '1',
+            name: 'Harbor Heights Hotel',
+            rating: '4',
+            center: 'Colorado',
+            distanceFromCenter: '4km',
+            sustainableLevel: '3',
+            beType: '1 extra-large double bed',
+            cost: '12000',
+            texes: '1200'
+        },
+        {
+            imageUrl: "https://images.unsplash.com/photo-1589705149724-1379ec03e196?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHZhY2F0aW9ufGVufDB8fDB8fHww",
+            id: '1',
+            name: 'Harbor Heights Hotel',
+            rating: '4',
+            center: 'Colorado',
+            distanceFromCenter: '4km',
+            sustainableLevel: '3',
+            beType: '1 extra-large double bed',
+            cost: '12000',
+            texes: '1200'
+        },
+        {
+            imageUrl: "https://images.unsplash.com/photo-1589705149724-1379ec03e196?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHZhY2F0aW9ufGVufDB8fDB8fHww",
+            id: '1',
+            name: 'Harbor Heights Hotel',
+            rating: '4',
+            center: 'Colorado',
+            distanceFromCenter: '4km',
+            sustainableLevel: '3',
+            beType: '1 extra-large double bed',
+            cost: '12000',
+            texes: '1200'
+        },
+        {
+            imageUrl: "https://images.unsplash.com/photo-1589705149724-1379ec03e196?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHZhY2F0aW9ufGVufDB8fDB8fHww",
+            id: '1',
+            name: 'Harbor Heights Hotel',
+            rating: '4',
+            center: 'Colorado',
+            distanceFromCenter: '4km',
+            sustainableLevel: '3',
+            beType: '1 extra-large double bed',
+            cost: '12000',
+            texes: '1200'
+        },
+        {
+            imageUrl: "https://images.unsplash.com/photo-1589705149724-1379ec03e196?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHZhY2F0aW9ufGVufDB8fDB8fHww",
+            id: '1',
+            name: 'Harbor Heights Hotel',
+            rating: '4',
+            center: 'Colorado',
+            distanceFromCenter: '4km',
+            sustainableLevel: '3',
+            beType: '1 extra-large double bed',
+            cost: '12000',
+            texes: '1200'
+        },
+        {
+            imageUrl: "https://images.unsplash.com/photo-1589705149724-1379ec03e196?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHZhY2F0aW9ufGVufDB8fDB8fHww",
+            id: '1',
+            name: 'Harbor Heights Hotel',
+            rating: '4',
+            center: 'Colorado',
+            distanceFromCenter: '4km',
+            sustainableLevel: '3',
+            beType: '1 extra-large double bed',
+            cost: '12000',
+            texes: '1200'
+        }
+    ]
+
+    const resultElements = results.map(item => (
+        <div className="single--result">
+            <img src={item.imageUrl} alt="" />
+            <div className="specs--container">
+                <div className="details--and-ratings">
+                    <div className="top--left">
+                        <div className="name--specs">
+                            <h3>{item.name}</h3>
+                            <span className="stars">
+                                <BsStarFill className="gold--stars"/>
+                                <BsStarFill className="gold--stars"/>
+                                <BsStarFill className="gold--stars"/>
+                                <BsStarFill className="gold--stars"/>
+                            </span>
+                        </div>
+                        <div className="location--specs">
+                            <Link>{item.center}</Link>
+                            <Link>Show on map</Link>
+                            <span>{item.distanceFromCenter} from centre</span>
+                        </div>
+                        <div className="sustainability--specs">
+                            <PiLeafFill className="green-icons"/>
+                            <PiLeafFill className="green-icons"/>
+                            <PiLeafFill className="green-icons"/>
+                            <span>Travel Sustainable Level 2</span>
+                        </div>
+                    </div>
+                    <div className="top--right">
+                        <div>
+                            <p>Good</p>
+                            <small>1,000 reviews</small>
+                        </div>
+                        <span>{item.rating}</span>
+                    </div>
+                </div>
+                <div className="room--and--charges">
+                    <div className="room--card">
+                        <p>King Room - Non-Smoking</p>
+                        <small> 1 extra-large double bed</small>
+                        <div className="green--text">
+                            <p>Breakfast included</p>
+                            <div className="perks">
+                            <TiTick/>
+                            <p>
+                            Free cancellation
+                            </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="charges">
+                    <small>1 night, 2 adults</small>
+                        <span className="main--cost">KES 16,290</span>
+                        <small className="taxes">
+                        +KES 1,905 taxes and charges
+                        </small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    ))
+    return (
+        <div className="searchresults--page">
+            <div className="searchresults--page-container">
+                <div className="filters--column"></div>
+                <div className="results--column">
+                    {resultElements}
+                </div>
+            </div>
+        </div>
+    )
+}
