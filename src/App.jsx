3d35ad7717b53cddeb765641575d3028868cf060
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-04 23:59:31 +0300

    updated component logic

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,27 @@ import {
 import './App.css'
 import Layout from './components/layout/Layout'
 import Home from './pages/home/Home'
-import Searchresults from './pages/searchresults/Searchresults'
+import Hotels from './pages/hotels/Hotels'
+import HotelLayout from './components/hotelLayout/HotelLayout'
+import Overview from './pages/hotel/overview/Overview'
+import InfoAndPrices from './pages/hotel/infoAndPrices/InfoAndPrices'
+import Facilities from './pages/hotel/facilities/facilities'
+import Rules from './pages/hotel/rules/Rules'
+import Reviews from './pages/hotel/reviews/Reviews'
 
 function App() {
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path='/' element={<Layout/>}>
       <Route index element={<Home/>}/>
-      <Route path='searchresults' element={<Searchresults/>}/>
+      <Route path='hotels' element={<Hotels/>}/>
+      <Route path='hotels/:id' element={<HotelLayout/>}>
+        <Route index element={<Overview/>}/>
+        <Route path='infoAndPrices' element={<InfoAndPrices/>}/>
+        <Route path='facilities' element={<Facilities/>}/>
+        <Route path='rules' element={<Rules/>}/>
+        <Route path='reviews' element={<Reviews/>}/> 
+      </Route>
+      
     </Route>
   ))
 
