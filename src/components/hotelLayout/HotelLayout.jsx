6633e9de5d7488b578184c46d18d375eb9664874
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-06 23:03:54 +0300

    added build component

---
+++ src/components/hotelLayout/HotelLayout.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import './hotelLayout.css'
+
+export default function HotelLayout() {
+    const activeStyle = {
+        textDecoration: "underline"
+    }
+    return (
+        <div className="hotelLayout--container">
+           <div className="over-view--head">
+                            <div className="hotel--nav">
+                                <NavLink
+                                   style={({isActive}) => isActive ? activeStyle : null} 
+                                   to='.'
+                                >Overview</NavLink>
+                                <NavLink
+                                   style={({isActive}) => isActive ? activeStyle : null} 
+                                   to='infoAndPrices'
+                                >Info and prices</NavLink>
+                                <NavLink
+                                   style={({isActive}) => isActive ? activeStyle : null} 
+                                   to='facilities'
+                                >Facilities</NavLink>
+                                <NavLink
+                                   style={({isActive}) => isActive ? activeStyle : null} 
+                                   to='rules'
+                                >House Rules</NavLink>
+                                <NavLink
+                                   style={({isActive}) => isActive ? activeStyle : null} 
+                                   to='reviews'
+                                >Giest Reviews(2800)</NavLink>
+                            </div>
+                           
+                        </div>
+            <Outlet />
+        </div>
+    )
+}
