3125a95a3d76f2d7777d8a4490c515416cb22577
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-08 23:12:59 +0300

    added final step booking page

---
+++ src/pages/booking/finalStep/FinalStep.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import './finalStep.css'
+
+import BookingHotelDetails from "../../../components/bookingHotelDetails/BookingHotelDetails";
+import ReservedBookingDetails from "../../../components/reservedBookingDetails/ReservedBookingDetails";
+import ReservedBookingPriceSummary from "../../../components/reservedBookingPriceSummary/ReservedBookingPriceSummary";
+import ReservedCancellPolicy from "../../../components/reservedCancellPolicy/ReservedCancellPolicy";
+import PromoCode from "../../../components/promoCode/PromoCode";
+
+
+export default function FinalStep () {
+    return (
+        <div className="finalStep--page">
+            <div className="finalStep--page--container">
+                <div className="selected--hotel--details">
+                    <BookingHotelDetails/>
+                    <ReservedBookingDetails/>
+                    <ReservedBookingPriceSummary/>
+                    <ReservedCancellPolicy/>
+                    <PromoCode/>
+
+                </div>
+                <div className="user--payment--details">
+
+                </div>
+            </div>
+        </div>
+    )
+}
