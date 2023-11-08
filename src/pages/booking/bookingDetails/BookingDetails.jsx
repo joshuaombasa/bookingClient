3da30d9257c11aafb5578518609b74069fb0fab6
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-08 23:14:32 +0300

    added final step pagee

--- src/pages/booking/bookingDetails/BookingDetails.jsx
+++ src/pages/booking/bookingDetails/BookingDetails.jsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import './bookingDetails.css'
 
 import { Link } from "react-router-dom";
+import { FiChevronRight } from 'react-icons/fi'
 import BookingHotelDetails from "../../../components/bookingHotelDetails/BookingHotelDetails";
 import ReservedBookingDetails from "../../../components/reservedBookingDetails/ReservedBookingDetails";
 import ReservedBookingPriceSummary from "../../../components/reservedBookingPriceSummary/ReservedBookingPriceSummary";
 import ReservedCancellPolicy from "../../../components/reservedCancellPolicy/ReservedCancellPolicy";
 import ReserveUserForm from "../../../components/reserveUserForm/ReserveUserForm";
+import ReservedRoomDetails from "../../../components/reservedRoomDetails/ReservedRoomDetails";
 
 export default function BookingDetails() {
 
@@ -23,8 +25,13 @@ export default function BookingDetails() {
                 </div>
                 <div className="your--personal--details">
                     <ReserveUserForm/>
+                    <ReservedRoomDetails/>
                 </div>
             </div>
+            <Link to='/reserve/payment' className="next--page--link">
+                <span>Next page</span>
+                <span><FiChevronRight className="icon"/></span>
+            </Link>
         </div>
     )
 }
