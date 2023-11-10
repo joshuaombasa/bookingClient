514ac056d85741c7d5ac6916ded127def773f7e6
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-10 22:46:22 +0300

    updated root routing logic

--- src/App.jsx
+++ src/App.jsx
@@ -17,29 +17,43 @@ import Reviews from './pages/hotel/reviews/Reviews'
 import ReserveLayout from './components/reserverLayout/ReserveLayout'
 import BookingDetails from './pages/booking/bookingDetails/BookingDetails'
 import FinalStep from './pages/booking/finalStep/FinalStep'
+import EmailForm from './pages/signup/emailForm/EmailForm'
+import PasswordForm from './pages/signup/passwordForm/PasswordForm'
+import RegisterLayout from './components/registerLayout/RegisterLayout'
+import LoginLayout from './components/loginLayout/LoginLayout'
+import LoginEmailForm from './pages/login/loginEmailForm/LoginEmailForm'
+import LoginPasswordForm from './pages/login/loginPasswordForm/LoginPasswordForm'
 
 function App() {
   const router = createBrowserRouter(createRoutesFromElements(
-    <Route path='/' element={<Layout/>}>
-      <Route index element={<Home/>}/>
-      <Route path='hotels' element={<Hotels/>}/>
-      <Route path='hotels/:id' element={<HotelLayout/>}>
-        <Route index element={<Overview/>}/>
-        <Route path='infoAndPrices' element={<InfoAndPrices/>}/>
-        <Route path='facilities' element={<Facilities/>}/>
-        <Route path='rules' element={<Rules/>}/>
-        <Route path='reviews' element={<Reviews/>}/> 
+    <Route path='/' element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path='hotels' element={<Hotels />} />
+      <Route path='hotels/:id' element={<HotelLayout />}>
+        <Route index element={<Overview />} />
+        <Route path='infoAndPrices' element={<InfoAndPrices />} />
+        <Route path='facilities' element={<Facilities />} />
+        <Route path='rules' element={<Rules />} />
+        <Route path='reviews' element={<Reviews />} />
       </Route>
-      <Route path='reserve' element={<ReserveLayout/>}>
-        <Route index element={<BookingDetails/>}/>
-        <Route path='payment' element={<FinalStep/>}/>
+      <Route path='reserve' element={<ReserveLayout />}>
+        <Route index element={<BookingDetails />} />
+        <Route path='payment' element={<FinalStep />} />
+      </Route>
+      <Route path='register' element={<RegisterLayout />}>
+        <Route index element={<EmailForm />} />
+        <Route path='password' element={<PasswordForm />} />
+      </Route>
+      <Route path='login' element={<LoginLayout/>}>
+        <Route index element={<LoginEmailForm/>}/>
+        <Route path='password' element={<LoginPasswordForm/>}/>
       </Route>
     </Route>
   ))
 
   return (
     <>
-     <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </>
   )
 }
