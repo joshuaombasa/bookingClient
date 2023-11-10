598720471fd56bd205a64d22c5c1ee1e8239965e
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-10 22:54:16 +0300

    added build component

---
+++ src/components/loginLayout/LoginLayout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import './loginLayout.css'
+
+
+export default function LoginLayout() {
+    return (
+        <div className="registerLayout--container">
+            <Outlet />
+            <span className="line--light"></span>
+            <p>By signing in or creating an account, you agree with our Terms & conditions and Privacy statement</p>
+            <span className="line--light"></span>
+            <p>All rights reserved.</p>
+            <p>Copyright (2006 - 2023) - Booking.com™</p>
+        </div>
+    )
+}
