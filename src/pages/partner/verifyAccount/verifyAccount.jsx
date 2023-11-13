9de53b2f5029319d0dc07795a49b33f5d2f950ac
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-13 23:56:59 +0300

    added build page

---
+++ src/pages/partner/verifyAccount/verifyAccount.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './verifyAccount.css'
+
+
+export default function VerifyAccount() {
+    return (
+        <div className="verifyAccount--page">
+            <div className="verifyAccount--page--container">
+                <h3>Verify your account</h3>
+                <div className="">
+                    <p>We sent you an email with a verification link to</p>
+                    <p><strong>oteriomasa@gmail.com.</strong></p>
+                    <p>To confirm your account please follow the link in the email we just sent.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
