ca5d829b534d42b24f3a1152cda88cbf6a6bd04a
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-14 23:29:57 +0300

    added build page

---
+++ src/pages/partner/group/groupReservations/GroupReservations.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import './groupReservations.css'
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+
+export default function GroupReservations() {
+
+    const [showMoreFilters, setShowMoreFilters] = useState(false)
+
+    function toggleShowMoreFilters() {
+        setShowMoreFilters(prevState => !prevState)
+    }
+
+    return (
+        <div className="groupReservations--page">
+            <div className="groupReservations--page--container">
+                <h1>Reservations</h1>
+                <div className="filters">
+                    <div className="action--filter">
+                        <label htmlFor="dateOf">Date of</label>
+                        <select name="" id="dateOf">
+                            <option value="">Reservation</option>
+                            <option value="">Check-in</option>
+                            <option value="">Check-out</option>
+                        </select>
+                    </div>
+                    <div className="dates--filter">
+                        <label htmlFor="dateValue">Filter by dates</label>
+                        <input type="date" name="" id="dateValue" />
+                    </div>
+                    <div
+                        className="more--filters"
+                        onClick={() => toggleShowMoreFilters()}
+                    >
+                        <p>More filters</p>
+                        <span>
+                            {showMoreFilters ? <IoIosArrowDown /> : <IoIosArrowUp />}
+                        </span>
+                    </div>
+                    <button>Show reservations</button>
+                </div>
+                {showMoreFilters && <div className="more--filters--container">
+                    <div className="filter--by--property">
+                        <label htmlFor="">Properties</label>
+                        <select name="" id="">
+                            <option value="">Sunset Residences</option>
+                            <option value="">Gretsa View</option>
+                            <option value="">Silver Springs</option>
+                            <option value="">Green Valley Estates</option>
+                            <option value="">Majestic Heights</option>
+                        </select>
+                    </div>
+                    <div className="filter--byt--reservation--status">
+                        <h3>Reservation status</h3>
+                        <div className="status--checks">
+                            <div className="">
+                                <input type="checkbox" name="" id="" />
+                                <label htmlFor="">Ok</label>
+                            </div>
+                            <div className="">
+                                <input type="checkbox" name="" id="" />
+                                <label htmlFor="">Cancelled</label>
+                            </div>
+                            <div className="">
+                                <input type="checkbox" name="" id="" />
+                                <label htmlFor="">No show</label>
+                            </div>
+                            <div className="">
+                                <input type="checkbox" name="" id="" />
+                                <label htmlFor="">Paid online</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>}
+                <div className="reservations--table">
+                    <div className="reservations--table--head">
+                        <p>Property ID</p>
+                        <p className="property--name">Property name</p>
+                        <p>Location</p>
+                        <p>Guest name</p>
+                        <p>Check-in</p>
+                        <p>Check-out</p>
+                        <p>Status</p>
+                        <p>Total payment</p>
+                        <p>Commission</p>
+                        <p>Reservation number</p>
+                        <p>Booked on</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
