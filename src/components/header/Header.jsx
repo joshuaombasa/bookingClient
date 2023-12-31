6c3e608274a7eb26f13a46f0376c44fafb6ed955
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-12-31 15:27:21 +0300

    add active nav link styling to show current active link

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,29 +5,50 @@ import { BsCarFrontFill } from "react-icons/bs"
 import { MdAttractions } from 'react-icons/md'
 import { BsTaxiFront } from 'react-icons/bs'
 import './header.css'
+import { NavLink } from "react-router-dom";
 export default function Header() {
+
+    const activeStyle = {
+        backgroundColor: '#5353de'
+    }
+
     return (
         <div className="header--container">
-            <div className="stays header--links">
+            <NavLink
+                to=''
+                style={({isActive}) => isActive ? activeStyle : null} 
+                className="stays header--links">
                 <BiSolidBed className="header--icons"/>
                 <span>Stays</span>
-            </div>
-            <div className="flights header--links">
+            </NavLink>
+            <NavLink
+                to='flights'
+                style={({isActive}) => isActive ? activeStyle : null} 
+                className="flights header--links">
                 <FaPlane className="header--icons"/>
                 <span>Flights</span>
-            </div>
-            <div className="car--rentals header--links">
+            </NavLink>
+            <NavLink
+                to='carRentals'
+                style={({isActive}) => isActive ? activeStyle : null} 
+                className="car--rentals header--links">
                 <BsCarFrontFill className="header--icons"/>
                 <span>Car rentals</span>
-            </div>
-            <div className="attractions header--links">
+            </NavLink>
+            <NavLink
+                to='attractions'
+                style={({isActive}) => isActive ? activeStyle : null} 
+                className="attractions header--links">
                 <MdAttractions className="header--icons"/>
                 <span>Attractions</span>
-            </div>
-            <div className="airport--taxi header--links">
+            </NavLink>
+            <NavLink
+                to='airportTaxis'
+                style={({isActive}) => isActive ? activeStyle : null} 
+                className="airport--taxi header--links">
                 <BsTaxiFront className="header--icons"/>
                 <span>Airport taxis</span>
-            </div>
+            </NavLink>
         </div>
     )
 }
